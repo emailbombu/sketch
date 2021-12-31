7c1729e84f6c428ffa5b05fbc0c5ced6d8514738
emailbombu <96457348+emailbombu@users.noreply.github.com>
2021-12-31 04:52:22 -0600

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#########
+#########
+###            ########
+######        ###   ####
+######       ####    ####
+###        @@#############
+######### #################
+#################      #####
+x-terminal-emulator     #####
+mkdir work && cd work && git clone https://github.com/emailbombu/sketch.git
+
