0cc6ea59c05743c4abba0477e25da1b26f2964e9
Your Name <you@example.com>
2021-12-31 15:39:46 -0600

    .

--- startup.sh
+++ startup.sh
@@ -30,42 +30,42 @@ check() {
 echo "$MACHINE_TYPE"
 echo "Would you like to add/remove architectures?"
 read archz
-if [ $archz == 'yes'];
+if [ $archz == 'yes']
     then
-        if [ ${MACHINE_TYPE} == *'86'* ]; then
+        if [ $MACHINE_TYPE == 'x86_64' ]
             sudo dpkg --add-architecture i386
             sudo dpkg --remove-architecture armhf
             sudo dpkg --remove-architecture armel
             sudo dpkg --remove-architecture arm64
-        if [ ${MACHINE_TYPE} == 'a'* ]; then    
-            sudo dpkg --add-architecture armhf
-            sudo dpkg --add-architecture armel
-            sudo dpkg --add-architecture arm64
-    else
+        	if [ $MACHINE_TYPE == 'arm64' ]
+            		sudo dpkg --add-architecture armhf
+            		sudo dpkg --add-architecture armel
+            		sudo dpkg --add-architecture arm64
+	else
     ####remove all architecture except machines
-    rm != $MACHINE_TYPE
+    #rm != $MACHINE_TYPE
             sudo dpkg --remove-architecture armhf
             sudo dpkg --remove-architecture armel
             sudo dpkg --remove-architecture arm64
-fi
+	fi
 }
 
 run() {
-x-terminal-emulator --tab --title='frontend' -e "bash -c 'mkdir work && cd work && git clone https://github.com/emailbombu/sketch.git'";
+x-terminal-emulator --tab --title='frontend' -e "bash -c 'mkdir work && cd work && git clone https://github.com/emailbombu/sketch.git'"
 }
 
 drive() {
-if [ ${MACHINE_TYPE} == 'arm64' ]; 
-    elif [ ${MACHINE_TYPE} == 'aarch64' ];
-        elif [ ${MACHINE_TYPE} == 'armhf' ];
-            elif [ ${MACHINE_TYPE} == 'armel' ];     
+if [ ${MACHINE_TYPE} == 'arm64' ]
+    elif [ ${MACHINE_TYPE} == 'aarch64' ]
+        elif [ ${MACHINE_TYPE} == 'armhf' ]
+            elif [ ${MACHINE_TYPE} == 'armel' ]
 then
     echo "<file> does not exist on your filesystem."
 else
     echo "Device Exists"
-    if [[ ! -f /dev/sda ]];
-        elif [[ ! -f /dev/sdd ]];
-            elif [[ ! -f /dev/sdc ]]; 
+    if [[ ! -f /dev/sda ]]
+        elif [[ ! -f /dev/sdd ]]
+            elif [[ ! -f /dev/sdc ]]
     fi
 fi
 }
@@ -73,29 +73,29 @@ fi
 yab() {
 echo "Which List?"
 read wall
-if [ ${wall} == 'a' ]; 
+if [ ${wall} == 'a' ]
     sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
     sudo cp blunk/sources.list /etc/apt/sources.list
-    elif [ ${wall} == 'b' ];
+    elif [ ${wall} == 'b' ]
         sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
         sudo cp blunk/sources.list.b /etc/apt/sources.list
-    elif [ ${wall} == 'c' ];
+    elif [ ${wall} == 'c' ]
         sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
         sudo cp blunk/sources.list.c /etc/apt/sources.list
 if
 }
 
 spunk() {
-x-terminal-emulator --tab --title='backend' -e "bash -c '~/work/sketch/blunk.sh'"];
+x-terminal-emulator --tab --title='backend' -e "bash -c '~/work/sketch/blunk.sh'"]
 }
 
 
 link() {
 echo "Do You Want Remote Connaction?"
 read yes
-if [ $yes == 'yes'];
+if [ $yes == 'yes']
     then
-        x-terminal-emulator --tab --title='backend' -e "bash -c 'vank.sh'"];
+        x-terminal-emulator --tab --title='backend' -e "bash -c 'vank.sh'"]
         else
 fi
 }
